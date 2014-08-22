526e92a942b604e81e1d223826a568649c1c0070
Eric <erjohnson.pdx@gmail.com>
2014-08-21 23:11:33 -0700

    Add main function

---
+++ fizzbuzz.rs
@@ -0,0 +1,26 @@
+fn main() {
+
+  fn div_by_three(num: int) -> bool {
+    num % 3 == 0
+  }
+
+  fn div_by_five(num: int) -> bool {
+    num % 5 == 0
+  }
+
+  fn div_by_fifteen(num: int) -> bool {
+    num % 15 == 0
+  }
+
+  for i in range(1i, 100) {
+    if div_by_fifteen(i) { 
+      println!("FizzBuzz"); 
+    } else if div_by_three(i) { 
+      println!("Fizz"); 
+    } else if div_by_five(i) { 
+      println!("Buzz"); 
+    } else { 
+      println!("{}", i);
+    }
+  }
+}
