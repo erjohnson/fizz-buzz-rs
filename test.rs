5716c73d24b9c0ee599373d55a7db3062b2add2e
Eric <erjohnson.pdx@gmail.com>
2014-08-21 22:43:06 -0700

    Refactor test divide by 3

--- test.rs
+++ test.rs
@@ -1,11 +1,18 @@
+fn div_by_three(num: int) -> bool {
+  num % 3 == 0
+}
+
 #[test]
 fn test_div_by_three() {
+    let num = 1;
+    if div_by_three(num) {
+        fail!("{} is not a multiple of 3", num);
+    }
+}
 
-  fn div_by_three(num: int) -> bool {
-   true
-  }
-
-  if div_by_three(1) {
-      fail!("One is not three");
-  }
+#[test]
+fn test_div_by_three_with_three() {
+    if !div_by_three(3) {
+        fail!("Three should be three");
+    }
 }
